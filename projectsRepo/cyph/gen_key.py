eccd1a481de352f260d08618a2f3741526f9f455
Ramingo93 <kklw93games@gmail.com>
2024-03-27 01:17:01 +0100

    removed extra folder

--- projectsRepo/cyph/gen_key.py
+++
@@ -1,5 +0,0 @@
-from cryptography.fernet import Fernet
-
-with open('K.key', 'wb') as f:
-    f.write(Fernet.generate_key())
-    
--- projectsRepo/cyph/main.py
+++
@@ -1,9 +0,0 @@
-import os
-
-targets = []
-
-for _, _, file_ in os.walk('targets'):
-    targets.append(file_)
-
-print(targets)
-input()
